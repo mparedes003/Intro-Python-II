9f6485a09610746809e09a4be43cc06c7b639f8b
mparedes003 <onnabugeishadev@gmail.com>
2019-10-02 09:49:39 -0700

    Create Room Class with name and description as initial attributues.

--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name                  # name of room
+        self.description = description    # what player sees and feels when in the room
