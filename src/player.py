3402bbc249609ab2183abd646bc211eac080d373
mparedes003 <onnabugeishadev@gmail.com>
2019-10-03 13:25:13 -0700

    Add items attribute to player. Add function that allows a player to pick up an item.

--- src/player.py
+++ src/player.py
@@ -1,11 +1,16 @@
+from item import Item
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
 
 class Player:
-    def __init__(self, name, location):
+    def __init__(self, name, items=None, location):
         self.name = name            # name of player
         self.location = location    # what room they are currently in
+        # If the player is not carrying any items
+        self.items = [] if items is None else items
+        # return an empty array
+        # else return the items the player is carrying
 
     def change_location(self, new_location):
         self.location = new_location
@@ -15,3 +20,8 @@ class Player:
 
     def __repr__(self):     # for programmers consumption
         return f"Player({repr(self.name)}, {repr(self.location)})"
+
+    # function that allows a player to pick up an item
+    def get(self, item):
+        # append method adds the item to the players inventory
+        self.items.append(item)
