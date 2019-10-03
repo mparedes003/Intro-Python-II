762257f047ba8169ac6646abf730a8e6b1f085b2
mparedes003 <onnabugeishadev@gmail.com>
2019-10-03 12:21:56 -0700

    Add items attribute  and list_items function to Room class.

--- src/room.py
+++ src/room.py
@@ -3,16 +3,17 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name                  # name of room
         self.description = description    # what player sees and feels when in the room
-        self.n_to = None
-        self.s_to = None
+        self.items = [] if items is None else items  # if there are no items in the room
+        self.n_to = None                                # return an empty array
+        self.s_to = None                              # else return the items in the room
         self.e_to = None
         self.w_to = None
 
     def __str__(self):      # for human consumption
-        return f"Room (name: {self.name})"
+        return f"Room (name: {self.name}, description: {self.description}, items: {self.items})"
 
     def __repr__(self):     # for programmers consumption
         return f"Room({repr(self.name)}, {repr(self.description)})"
@@ -28,3 +29,14 @@ class Room:
             return self.w_to
         else:
             return None
+
+    def __str__(self):      # for human consumption
+        return f"Room (name: {self.name}, description: {self.description}, items: {self.items})"
+
+    # function that lists items that can be found in the room
+    def list_items(self):
+        # print with f string
+        # use the join method on a comma as an items separator
+            #  use a for loop to loop over the item list
+        print(
+            f"\n This room contains: {', '.join(item.name for item in self.items)}")
