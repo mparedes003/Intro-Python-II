c7f55c9ecbab7d9e7f59609e96d3f400e17dba8b
mparedes003 <onnabugeishadev@gmail.com>
2019-10-02 22:09:03 -0700

    Add __str__  and  __repr__ functions to Player class.

--- src/player.py
+++ src/player.py
@@ -8,3 +8,9 @@ class Player:
 
     def change_location(self, new_location):
         self.location = new_location
+
+    def __str__(self):      # for human consumption
+        return f"Player (location: {self.location})"
+
+    def __repr__(self):     # for programmers consumption
+        return f"Player(location: {self.name})"
